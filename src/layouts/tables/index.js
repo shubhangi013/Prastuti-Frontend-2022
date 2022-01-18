a0c9720f6f5109bad179d9bceba1caff40d065bd
shubhangi013 <choudharyshubhangi13@gmail.com>
2022-01-19 00:44:31 +0530

    removed the carousel

--- src/layouts/tables/index.js
+++ src/layouts/tables/index.js
@@ -1,5 +1,4 @@
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+
 
 // @mui material components
 import Grid from "@mui/material/Grid";
@@ -17,34 +16,10 @@ import DataTable from "examples/Tables/DataTable";
 // Data
 import authorsTableData from "layouts/tables/data/authorsTableData";
 import projectsTableData from "layouts/tables/data/projectsTableData";
-// import LeaderBoardCarousel from "examples/Carousel";
-
-
-
-
 
 function Tables() {
   const { columns, rows } = authorsTableData();
   const { columns: pColumns, rows: pRows } = projectsTableData();
-  const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-};
 
   return (
     <DashboardLayout>
@@ -78,23 +53,26 @@ function Tables() {
               </MDBox>
             </Card>
           </Grid>
+
+
           <Grid item xs={12}>
-            
+
+            <Card>
               <MDBox
                 mx={2}
                 mt={-3}
                 py={3}
                 px={2}
                 variant="gradient"
-                bgColor="info"
+                bgColor="success"
                 borderRadius="lg"
                 coloredShadow="info"
               >
-                <MDTypography variant="h6" color="white">
+                <MDTypography variant="h6" color="white" >
                   Event 1 Leaderboard
                 </MDTypography>
               </MDBox>
-              {/* <MDBox pt={2}>
+              <MDBox pt={2}>
                 <DataTable
                   table={{ columns: pColumns, rows: pRows }}
                   isSorted={false}
@@ -102,72 +80,10 @@ function Tables() {
                   showTotalEntries={false}
                   noEndBorder
                 />
-              </MDBox> */}
-            
- <Carousel
-  swipeable="false"
-  draggable="false"
-  showDots="true"
-  responsive={responsive}
-  ssr="true" // means to render carousel on server-side.
-  infinite="true"
-  // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-  autoPlaySpeed={1000}
-  keyBoardControl="true"
-  customTransition="all .5"
-  transitionDuration={500}
-  containerClass="carousel-container"
-  removeArrowOnDeviceType={["tablet", "mobile"]}
-  // deviceType={this.props.deviceType}
-  dotListClass="custom-dot-list-style"
-  itemClass="carousel-item-padding-40-px"
- 
->
-  <div> 
-    <Card sx={{gap:40, margin: "0 20px"}}>    <DataTable
-                  table={{ columns: pColumns, rows: pRows }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
-    </Card>
- 
-    </div>
-  <div>
-    <Card sx={{gap:40, margin: "0 20px"}}> 
-           <DataTable
-                  table={{ columns: pColumns, rows: pRows }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
-    </Card>
-    </div>
-  <div>
-    <Card sx={{gap:40, margin: "0 20px"}}> 
-           <DataTable
-                  table={{ columns: pColumns, rows: pRows }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
-    </Card>
-    </div>
-  <div>
-    <Card sx={{gap:40, margin: "0 20px"}}> 
-           <DataTable
-                  table={{ columns: pColumns, rows: pRows }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
-    </Card>
-    </div>
-</Carousel>
+              </MDBox>
+            </Card>
+
+
           </Grid>
 
         </Grid>
